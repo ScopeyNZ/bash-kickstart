5bc010ca2406aa6cad9fd69022e1d2fce1b85277
Guy Marriott <guy@scopey.co.nz>
2018-07-18 13:01:37 +1200

    Enhancements to the remote add script

--- scripts/remote-add.sh
+++ scripts/remote-add.sh
@@ -1,2 +1,50 @@
-DIR=$(basename $PWD)
-git remote add ${3:-cc} git@github.com:${2:-creative-commoners}/${1:-$DIR} ${@:4}
+HELP="Usage: `basename $0` [-hs]\n
+\n
+Options:\n
+	-h 	Show this help text\n
+	-s 	Skip checking the remote is valid"
+
+while getopts ":hs" opt; do
+  case $opt in
+    s)
+      SKIPCHECK=1
+      ;;
+ 	h)
+      echo -e $HELP
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      echo -e $HELP
+      exit 1
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+TEAM=${2:-creative-commoners}
+REMOTENAME=${1:-cc}
+REPO=$3
+
+if [[ -z $REPO ]] ; then
+	if ! ORIGIN=`git remote -v | head -1 | grep -oP '(?<=github.com[/:])([^/.]+/[^.]+.git)' | sed -E "s/[^\/]+/$TEAM/"`; then
+		echo "Could not determine remote. Please provide a remote: "
+		read REMOTE
+	else 
+		REMOTE="git@github.com:$ORIGIN"
+	fi
+else
+	REMOTE="git@github.com:$TEAM/$REPO"
+fi
+
+echo $ORIGIN
+
+HTTPSREMOTE=`echo $REMOTE | sed 's/git@github.com:/https:\/\/github.com\//'`
+
+if [[ $SKIPCHECK -ne 1 ]] && ! `git ls-remote -h $HTTPSREMOTE &> /dev/null` ; then
+	echo $HTTPSREMOTE" does not appear to be a valid remote. Maybe you should fork first (hub fork)"
+else
+	git remote add $REMOTENAME $REMOTE
+	echo "Added remote \"$REMOTENAME\": "$REMOTE
+fi
+
